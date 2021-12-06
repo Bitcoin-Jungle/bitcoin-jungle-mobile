1faca776efb5c22217ec6d4874c63afda7f173ba
Arjun Rawat <arjun.rawat0785@gmail.com>
2021-12-06 23:33:03 +0530

    webpack configuration changes.

--- webpack.config.js
+++ webpack.config.js
@@ -1,79 +1,98 @@
-const path = require('path');
+const path = require("path")
 
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
-const appDirectory = path.resolve(__dirname);
-const {presets} = require(`${appDirectory}/babel.config.js`);
+const appDirectory = path.resolve(__dirname)
+const { presets } = require(`${appDirectory}/babel.config.js`)
 
 const compileNodeModules = [
   // Add every react-native package that needs compiling
-  // 'react-native-gesture-handler',
-].map((moduleName) => path.resolve(appDirectory, `node_modules/${moduleName}`));
+  "@react-native-picker/picker",
+  "react-native-root-toast",
+  "react-native-snap-carousel",
+  "react-native-swiper",
+  "react-native-vector-icons",
+  "react-native-walkthrough-tooltip",
+  "victory-native",
+  "react-native-elements",
+  "react-native-image-picker",
+  "react-native-maps",
+  "react-native-qrcode-svg",
+  "react-native-ratings",
+  "react-native-root-siblings",
+  "react-native-version-number",
+  "@react-native-community/push-notification-ios",
+  "react-native-fingerprint-scanner",
+  'react-native-camera',
+  "@react-native-firebase/app",
+  '@react-native-firebase/analytics',
+  '@react-native-firebase/crashlytics',
+  '@react-native-firebase/messaging',
+  '@storybook/react-native',
+  'react-native-animatable',
+].map((moduleName) => path.resolve(appDirectory, `node_modules/${moduleName}`))
 
 const babelLoaderConfiguration = {
   test: /\.js$|tsx?$/,
   // Add every directory that needs to be compiled by Babel during the build.
   include: [
-    path.resolve(__dirname, 'index.web.js'), // Entry to your application
-    path.resolve(__dirname, './app.tsx'), // Change this to your main App file
-    path.resolve(__dirname, 'app'),
+    path.resolve(__dirname, "index.web.js"), // Entry to your application
+    path.resolve(__dirname, "./app.tsx"), // Change this to your main App file
+    path.resolve(__dirname, "app"),
     ...compileNodeModules,
   ],
   use: {
-    loader: 'babel-loader',
+    loader: "babel-loader",
     options: {
       cacheDirectory: true,
       presets,
-      plugins: ['react-native-web'],
+      plugins: ["react-native-web"],
     },
   },
-};
+}
 
 const svgLoaderConfiguration = {
   test: /\.svg$/,
   use: [
     {
-      loader: '@svgr/webpack',
+      loader: "@svgr/webpack",
     },
   ],
-};
+}
 
 const imageLoaderConfiguration = {
   test: /\.(gif|jpe?g|png|ttf)$/,
   use: {
-    loader: 'url-loader',
+    loader: "url-loader",
     options: {
-      name: '[name].[ext]',
+      name: "[name].[ext]",
     },
   },
-};
+}
 
 module.exports = {
   entry: {
-    app: path.join(__dirname, 'index.web.js'),
+    app: path.join(__dirname, "index.web.js"),
   },
   output: {
-    path: path.resolve(appDirectory, 'dist'),
-    publicPath: '/',
-    filename: 'rnw_blogpost.bundle.js',
+    path: path.resolve(appDirectory, "dist"),
+    publicPath: "/",
+    filename: "rnw_blogpost.bundle.js",
   },
   resolve: {
-    extensions: ['.web.tsx', '.web.ts', '.tsx', '.ts', '.web.js', '.js'],
+    extensions: [".web.tsx", ".web.ts", ".tsx", ".ts", ".web.js", ".js"],
     alias: {
-      'react-native$': 'react-native-web',
+      "react-native$": "react-native-web",
     },
+    fallback: { "stream": require.resolve("stream-browserify") }
   },
   module: {
-    rules: [
-      babelLoaderConfiguration,
-      imageLoaderConfiguration,
-      svgLoaderConfiguration,
-    ],
+    rules: [babelLoaderConfiguration, imageLoaderConfiguration, svgLoaderConfiguration],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'index.html'),
+      template: path.join(__dirname, "index.html"),
     }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
@@ -81,4 +100,4 @@ module.exports = {
       __DEV__: JSON.stringify(true),
     }),
   ],
-};
+}
